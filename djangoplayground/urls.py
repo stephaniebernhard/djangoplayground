dd6e0900b3d926574ce745b9fc60381ee6f2f185
Stephanie <fanny.bernhard@gmail.com>
2018-08-01 13:29:53 +0000

    beautify

--- djangoplayground/urls.py
+++ djangoplayground/urls.py
@@ -20,13 +20,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', 'blog.views.index'),
-    url(
-        r'^blog/view/(?P<slug>[^\.]+).html', 
-        'blog.views.view_post', 
-        name='view_blog_post'),
-    url(
-        r'^blog/category/(?P<slug>[^\.]+).html', 
-        'blog.views.view_category', 
-        name='view_blog_category'),
+    url(r'^blog/view/(?P<slug>[^\.]+).html', 'blog.views.view_post', name='view_blog_post'),
+    url(r'^blog/category/(?P<slug>[^\.]+).html', 'blog.views.view_category', name='view_blog_category'),
 ]
 
