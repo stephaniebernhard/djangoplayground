8d12ce57f28c3266b62e46cd12840aeddbeb1b6e
Stephanie <fanny.bernhard@gmail.com>
2018-08-01 12:42:12 +0000

    after hours and hours of trying..

--- djangoplayground/urls.py
+++ djangoplayground/urls.py
@@ -19,14 +19,14 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', 'blog.views.index'),
+    url(
+        r'^blog/view/(?P<slug>[^\.]+).html', 
+        'blog.views.view_post', 
+        name='view_blog_post'),
+    url(
+        r'^blog/category/(?P<slug>[^\.]+).html', 
+        'blog.views.view_category', 
+        name='view_blog_category'),
 ]
 
-(r'^$', 'djangorocks.blog.views.index'),
-url(
-    r'^blog/view/(?P<slug>[^\.]+).html', 
-    'djangorocks.blog.views.view_post', 
-    name='view_blog_post'),
-url(
-    r'^blog/category/(?P<slug>[^\.]+).html', 
-    'djangorocks.blog.views.view_category', 
-    name='view_blog_category'),
