ef2a3c579e1a7e33040bc3d5794e700a36c2f916
Stephanie <fanny.bernhard@gmail.com>
2018-08-01 12:35:25 +0000

    added urls

--- djangoplayground/urls.py
+++ djangoplayground/urls.py
@@ -20,3 +20,13 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+(r'^$', 'djangorocks.blog.views.index'),
+url(
+    r'^blog/view/(?P<slug>[^\.]+).html', 
+    'djangorocks.blog.views.view_post', 
+    name='view_blog_post'),
+url(
+    r'^blog/category/(?P<slug>[^\.]+).html', 
+    'djangorocks.blog.views.view_category', 
+    name='view_blog_category'),
